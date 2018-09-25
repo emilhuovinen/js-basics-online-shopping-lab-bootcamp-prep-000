32b7665233aed6efeff26c15d19f26af489f20c2
emil.huo <emil.huo@gmail.com>
2018-09-25 13:03:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ function removeFromCart(item) {
   for (var i = 0; i < cart.length; i++) {
     var getName = cart[i];
     if (getName.itemName === item) {
-      getName.splice(i, 1);
+      cart.splice(i, 1);
     }
   }
 }
